30eefe51c43016392f0dd1e1c7dd55d61103fffa
Lucas Schmutz <lucas.schmutz@unil.ch>
2023-02-20 10:39:21 +0100

    Dev of GraphCutOptimization.R

--- CMIP6_CDS_script_async.py
+++ CMIP6_CDS_script_async.py
@@ -1,7 +1,7 @@
+import os
 from concurrent.futures import ThreadPoolExecutor
 
 import cdsapi
-import os
 
 c = cdsapi.Client()
 
@@ -56,17 +56,14 @@ def main():
                 year_int = list(range(2015, 2101))
                 year = list(map(str, year_int))
             for i in model_list:
-                directory = os.path.abspath(f"download/{i}")
-                if not os.path.exists(directory):
-                    os.makedirs(directory)
-                    print(f"Created directory: {directory}")
                 for j in variable_list:
-                    name = f"download/{i}/{j}_Amon_{i}_{n}.zip"
-                    z += 1
-                    try:
-                        var = [exe.submit(cds_api_call, n, i, year, month, j, name)]
-                    finally:
-                        pass
+                    name = 'download/' + j + '_Amon_' + i + '_' + n + '.zip'
+                    if not os.path.exists(name):
+                        z += 1
+                        try:
+                            var = [exe.submit(cds_api_call, n, i, year, month, j, name)]
+                        finally:
+                            pass
 
 
 main()
