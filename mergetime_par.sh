242e4c9f7f5a8acdf8a092899253a05fcfd5ad70
Lucas Schmutz <lucas.schmutz@unil.ch>
2023-03-13 09:31:02 +0100

    Working on data pre-processing

--- mergetime_par.sh
+++ mergetime_par.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Define the root directory containing all the model folders
-root_dir="Y:/scratch_idyst/LucasSchmutz/MultivariateGraphCut_GCMs/download_day_unzip"
+root_dir="/mnt/scratch_idyst/LucasSchmutz/MultivariateGraphCut_GCMs/download_day_unzip"
 
 # Define the directory where the merged files will be stored
-merged_dir="Y:/scratch_idyst/LucasSchmutz/MultivariateGraphCut_GCMs/download_day_merged"
+merged_dir="/mnt/scratch_idyst/LucasSchmutz/MultivariateGraphCut_GCMs/download_day_merged"
 
 # Define the number of parallel processes to use
 nprocs=4
@@ -25,7 +25,9 @@ for model_dir in "$root_dir"/*; do
 
         # Construct the output directory path for the merged files
         output_dir="$merged_dir/$(basename "$model_dir")/$(basename "$var_exp_dir")"
-        mkdir -p "$output_dir"
+        if [ ! -d "$output_dir" ]; then
+            mkdir -p "$output_dir" || { echo "Error: failed to create output directory $output_dir"; exit 1; }
+        fi
 
         # Loop over all netCDF files in the variable/experiment subfolder
         input_files=()
@@ -35,19 +37,21 @@ for model_dir in "$root_dir"/*; do
             fi
         done
 
-        # Merge the netCDF files using cdo mergetime
+        # Merge the netCDF files using cdo mergetime in parallel
         output_filename="$(basename "${input_files[0]}")"
         output_filename="${output_filename/_19500101-/}"
         output_filename="${output_filename/_v/_merged_v}"
         output_path="$output_dir/$output_filename"
-        cdo mergetime "${input_files[@]}" "$output_path" &
+        echo "${input_files[@]}" | tr ' ' '\n' | \
+            parallel -j $nprocs --no-notice cdo mergetime {} "$output_path" || { echo "Error: failed to merge files for $var_exp_dir"; exit 1; }
 
-        # Limit the number of parallel processes
-        if [ $(jobs -r -p | wc -l) -ge $nprocs ]; then
-            wait -n
+        # Check if the output file was created successfully
+        if [ ! -f "$output_path" ]; then
+            echo "Error: output file $output_path was not created"
+            continue
         fi
-    done
-done
 
-# Wait for all background jobs to finish
-wait
+        # Set the permissions of the output file to match the input files
+        if [ ${#input_files[@]} -gt 0 ]; then
+            chown --reference="${input_files[0]}" "$output_path" || { echo "Error: failed to set owner for $output_path"; exit 1; }
+            chmod --reference="${input_files[0]}" "$
