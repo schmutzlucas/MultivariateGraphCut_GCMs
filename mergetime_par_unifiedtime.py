c8873875aad3085420c6eb3b8e196a6124c37d81
Lucas Schmutz <lucas.schmutz@unil.ch>
2023-03-16 11:28:42 +0100

    Documentation update

--- mergetime_par_unifiedtime.py
+++ mergetime_par_unifiedtime.py
@@ -1,17 +1,26 @@
 import os
-import shutil
 import xarray as xr
 import numpy as np
 from datetime import datetime
 from multiprocessing import Pool
+from cftime import DatetimeProlepticGregorian
 
 root_dir = 'Y:\LucasSchmutz\MultivariateGraphCut_GCMs\download_day_unzip'
 merged_dir = 'Y:\LucasSchmutz\MultivariateGraphCut_GCMs\download_day_merged_multithread'
 
+
 def preprocess(ds):
     ds = ds.copy()
+    time_units = "days since 1850-01-01"
+    calendar = "proleptic_gregorian"
+
+    ds['time'] = xr.coding.times.decode_cf_datetime(ds['time'],
+                                                    units=time_units,
+                                                    calendar=calendar)
     ds['time'] = ds.indexes['time'].normalize()
+
     return ds
+
 
 def merge_model_dir(model_dir):
     for var_exp_dir in os.listdir(os.path.join(root_dir, model_dir)):
@@ -22,9 +31,11 @@ def merge_model_dir(model_dir):
         os.makedirs(output_dir, exist_ok=True)
 
         input_files = []
-        for filename in os.listdir(os.path.join(root_dir, model_dir, var_exp_dir)):
+        for filename in os.listdir(
+                os.path.join(root_dir, model_dir, var_exp_dir)):
             if filename.endswith(".nc"):
-                input_file_path = os.path.join(root_dir, model_dir, var_exp_dir, filename)
+                input_file_path = os.path.join(root_dir, model_dir, var_exp_dir,
+                                               filename)
                 input_files.append(input_file_path)
 
         print(f"Merging {len(input_files)} files:")
@@ -33,9 +44,12 @@ def merge_model_dir(model_dir):
         print("Current time:", now)
 
         try:
-            ds = xr.open_mfdataset(input_files, combine='nested', concat_dim='time', preprocess=preprocess)
-            start_date = np.datetime_as_string(ds.time.values[0], unit='D').replace('-', '')
-            end_date = np.datetime_as_string(ds.time.values[-1], unit='D').replace('-', '')
+            ds = xr.open_mfdataset(input_files, combine='nested',
+                                   concat_dim='time', preprocess=preprocess)
+            start_date = np.datetime_as_string(ds.time.values[0],
+                                               unit='D').replace('-', '')
+            end_date = np.datetime_as_string(ds.time.values[-1],
+                                             unit='D').replace('-', '')
 
             input_filename = os.path.basename(input_files[0])
             var = input_filename.split("_")[0]
@@ -48,17 +62,21 @@ def merge_model_dir(model_dir):
             output_path = os.path.join(output_dir, output_filename)
             print(output_path)
             if os.path.exists(output_path):
-                print(f"Output file {output_filename} already exists, skipping...")
+                print(
+                    f"Output file {output_filename} already exists, skipping...")
             else:
                 ds.to_netcdf(output_path)
 
         except Exception as e:
             with open("failed_merge.log", "a") as f:
-                f.write(f"An error occurred while processing {input_files}: {e}\n")
+                f.write(
+                    f"An error occurred while processing {input_files}: {e}\n")
             print(f"An error occurred while processing {input_files}: {e}")
 
+
 if __name__ == '__main__':
-    model_dirs = [d for d in os.listdir(root_dir) if os.path.isdir(os.path.join(root_dir, d))]
+    model_dirs = [d for d in os.listdir(root_dir) if
+                  os.path.isdir(os.path.join(root_dir, d))]
     num_processes = 8
     pool = Pool(num_processes)
     pool.map(merge_model_dir, model_dirs)
