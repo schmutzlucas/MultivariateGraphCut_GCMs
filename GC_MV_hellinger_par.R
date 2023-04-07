bff7024dd646e6cce0a8bbd11c8818480005db7f
Lucas Schmutz <lucas.schmutz@unil.ch>
2023-04-07 23:22:51 +0200

    Hellinger dev

---
+++ GC_MV_hellinger_par.R
@@ -0,0 +1,57 @@
+# Install and load necessary libraries
+list_of_packages <- read.table("package_list.txt", sep="\n")$V1
+new.packages <- list_of_packages[!(list_of_packages %in% installed.packages()[,"Package"])]
+if(length(new.packages))
+  install.packages(new.packages, repos = "https://cran.us.r-project.org")
+
+library(devtools)
+lapply(list_of_packages, library, character.only = TRUE)
+install_github("thaos/gcoWrapR")
+
+# Import libraries
+#library(c(list_of_packages))
+
+
+# Loading local functions
+
+source_code_dir <- 'functions/' #The directory where all functions are saved.
+file_paths <- list.files(source_code_dir, full.names = T)
+for(path in file_paths){source(path)}
+
+# Setting global variables
+lon <<- 0:359
+lat <<- -90:90
+# Temporal ranges
+year_present <<- 1979:1998
+year_future <<- 1999:2019
+# data directory
+data_dir <<- 'data/CMIP6/'
+
+# Bins for the kde
+nbins1d <<- 512
+
+# Period
+period <<- 'historical'
+
+# List of the variable used
+# variables <- c('tas', 'tasmax',
+#                'tasmin', 'pr',
+#                'psl', 'hur',
+#                'huss')
+variables <- c('pr', 'tas', 'tasmax')
+
+
+# Obtains the list of models from the model names or from a file
+# Method 1
+# TODO This needs ajustements to remove prefixes and suffixes
+dir_path <- paste0('data/CMIP6/')
+model_names <- list.dirs(dir_path, recursive = FALSE)
+model_names <- basename(model_names)
+
+tmp <- OpenAndKDE1D_betterPar(
+  model_names, variables, year_present, year_future, period
+)
+
+
+
+
---
+++ functions/Open_withprogress.R
@@ -0,0 +1,130 @@
+# Custom progress function
+progress_function <- function(n) {
+  cat(sprintf("Processing grid point %d of %d\n", n, length(lon)))
+  return(NULL)
+}
+
+OpenAndKDE1D_betterPar <- function (model_names, variables,
+                                    year_present, year_future, period) {
+
+
+  lat <- lat
+  lon <- lon
+  nbins1d <- nbins1d
+
+  registerDoSEQ(.progress = progress_function)
+
+  # Initialize data structures
+  kde_matrix <- array(0, c(length(lon), length(lat), nbins1d,
+                           length(model_names), length(variables)))
+
+  # Loop through variables and models
+  v <- 1
+  for(var in variables){
+    m <- 1
+    for(model_name in model_names){
+      dir_path <- paste0(data_dir, model_name, '/', var, '/')
+      # Create the pattern
+      pattern <- glob2rx(paste0(var, "_", model_name, "_", period, "*.nc"))
+
+      # Get the filepath
+      file_name <- list.files(path = dir_path,
+                              pattern = pattern)
+      file_path <- paste0(dir_path, file_name)
+      print(file_path)
+
+      # Check that there is only one matching file
+      if (length(file_path) == 1) {
+        nc <- nc_open(file_path)
+        # Extract and average data for present
+        yyyy <- substr(as.character(nc.get.time.series(nc)), 1, 4)
+        iyyyy <- which(yyyy %in% year_present)
+
+        # Read data from NetCDF file for the entire grid
+        grid_data <- ncvar_get(nc, var, start = c(1, 1, min(iyyyy)), count = c(-1, -1, length(iyyyy)))
+
+        # For each grid point...
+        foreach(i = iter(seq_along(lon)), .combine = 'cbind',
+                .packages = c("ncdf4"), .export = c("grid_data", "min", "iyyyy", "nbins1d", "m", "var")) %dopar% {
+          temp_result <- array(0, c(length(lat), nbins1d, length(model_names), length(variables)))
+          for (j in seq_along(lat)){
+            tmp <- grid_data[i, j, ]
+            if(var == 'pr' ) {
+              # Changing units to mm/day and log transform
+              tmp <- log10((tmp * 86400) + 1)
+            }
+
+            # For the first model
+            if(m == 1 ) {
+              # Compute the range of the variable for grid-point i-j
+              range_var <- range(tmp)
+              # Calculate the 10% margin
+              margin <- diff(range_var) * 0.1
+
+              # Set the lower and upper bounds with a 10% margin
+              range_var[1] <- range_var[1] - margin
+              range_var[2] <- range_var[2] + margin
+            }
+
+            # TODO should we fix the bandwidth?
+            dens_tmp <- density(tmp,
+                                from = range_var[1], to = range_var[2],
+                                n = nbins1d)
+
+            temp_result[j, , , ] <- dens_tmp$y
+          }
+          temp_result
+        } -> kde_matrix_sub
+
+        kde_matrix[, , , m, v] <- kde_matrix_sub
+
+        # Create dimensions and initialize matrices for present and future data if this is the first model and variable
+        if(j == 1 && i == 1) {
+          lat <- ncvar_get(nc, "lat")
+          lon <- ncvar_get(nc, "lon")
+          data_matrix$present <- data_matrix$future <-
+            array(0, c(length(lon), length(lat),
+                       length(model_names), length(variables)))
+        }
+
+        # Extract and average data for present
+        yyyy <- substr(as.character(nc.get.time.series(nc)), 1, 4)
+        iyyyy <- which(yyyy %in% year_present)
+        tmp <- apply(
+          ncvar_get(nc, var, start = c(1, 1, min(iyyyy)), count = c(-1, -1, length(iyyyy))),
+          1:2,
+          mean
+        )
+        data_matrix$present[, , i, j] <- tmp
+        data[['present']][[var]][[model_name]] <- tmp
+
+        # Extract and average data for future
+        iyyyy <- which(yyyy %in% year_future)
+        tmp <- apply(
+          ncvar_get(nc, var, start = c(1, 1, min(iyyyy)), count = c(-1, -1, length(iyyyy))),
+          1:2,
+          mean
+        )
+        data_matrix$future [, , i, j] <- tmp
+        data[['future']][[var]][[model_name]] <- tmp
+
+        # Close the file
+        nc_close(nc)
+
+      } else {
+        # Handle the case where there are multiple or no matching files
+        print("Error: Found multiple or no matching files")
+      }
+      # Update counter for models
+      m <- m + 1
+    }
+    # Update counter for variables
+    v <- v + 1
+  }
+
+  # ...
+
+  # Return the output as a list of two elements
+  output <- list(data, data_matrix, kde_matrix)
+  return(output)
+}
