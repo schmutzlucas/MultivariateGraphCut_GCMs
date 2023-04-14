b74e41e44922a76d3a87fb00b8893dc577fc64c6
Lucas Schmutz <lucas.schmutz@unil.ch>
2023-04-14 10:54:01 +0200

    Working on hellinger GC

--- ERA5_daily_stats.py
+++ ERA5_daily_stats.py
@@ -3,8 +3,8 @@ import cdsapi
 import os
 import calendar
 
-dir = r'/mnt/w\LucasSchmutz\MultivariateGraphCut_GCMs'
-#dir = r'w:\LucasSchmutz\mHM\volta_data\raw_data\era5'
+dir = r'/mnt/w/LucasSchmutz/MultivariateGraphCut_GCMs/data/ERA5'
+#dir = r'w:\LucasSchmutz\MultivariateGraphCut_GCMs'
 os.chdir(dir)
 
 c = cdsapi.Client()
@@ -35,7 +35,7 @@ months = list(map(str, months_int))
 
 month_31d = ['1','3','5','7','8','10','12']
 
-time = ['00:00','01:00','02:00','03:00','04:00','05:00','06:00','07:00','08:00','09:00','10:00','11:00', 
+time = ['00:00','01:00','02:00','03:00','04:00','05:00','06:00','07:00','08:00','09:00','10:00','11:00',
         '12:00','13:00','14:00','15:00','16:00','17:00','18:00','19:00','20:00','21:00','22:00','23:00']
 
 # Define the main function to run the data retrieval
