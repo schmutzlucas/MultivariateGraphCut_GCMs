25d5f597cce604a607bd656d99b5bb826816b7bb
Lucas Schmutz <lucas.schmutz@unil.ch>
2023-06-29 11:35:45 +0200

    trimming

--- CMIP6_CDS_script_async_daily.py
+++ CMIP6_CDS_script_async_daily.py
@@ -29,7 +29,7 @@ with open('model_list.txt', 'r') as file:
     model_list = file.read().splitlines()
 
 # Define the list of variables to retrieve
-variable_list = ['near_surface_wind_speed']
+variable_list = ['2m_temperature']
 
 # Define the list of experiments to retrieve data for
 experiment = ['historical', 'ssp5_8_5']
