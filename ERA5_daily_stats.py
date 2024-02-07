121256f9c96ba0c1bcd734442b52fbdb8abbb5bb
Lucas Schmutz <lucas.schmutz@unil.ch>
2024-02-07 14:35:26 +0100

    Updated the code to work with the new api version (maybe :))
    Signed-off-by: Lucas Schmutz <lucas.schmutz@unil.ch>

--- ERA5_daily_stats.py
+++ ERA5_daily_stats.py
@@ -56,7 +56,7 @@ VARIABLES = ['2m_temperature', 'total_precipitation', 'mean_sea_level_pressure']
 
 # Define the list of years to retrieve data for
 # Define the list of years to retrieve data for
-YEARS = list(map(str, range(1950, 2024)))
+YEARS = list(map(str, range(1950, 2025)))
 
 
 # Define the list of months to retrieve data for
